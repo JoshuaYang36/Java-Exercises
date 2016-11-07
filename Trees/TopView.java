254bd0c80cdb58623a07da58c61e888d40a01bea
Josh Yang <Jpy@uoregon.edu>
2016-11-07 15:55:15 -0800

    TopView

---
+++ Trees/TopView.java
@@ -0,0 +1,39 @@
+/*  ----------------------------------------------------------------------------------------------------------------/                                                                                                               
+/   Problem: Print all left nodes from bottom up, followed by all right node from top down                          /
+/                                                                                                                   /
+/ Node is defined as:                                                                                               /
+/           class Node {                                                                                            /
+/             int data;                                                                                             /
+/             Node left;                                                                                            /
+/             Node right;                                                                                           /
+/           }                                                                                                       /
+/   ---------------------------------------------------------------------------------------------------------------*/
+
+void top_view(Node root)
+{
+
+    if(root != null) {
+
+        top_view(root.left, true);
+
+        System.out.print(root.data + " ");
+
+        top_view(root.right, false);
+
+    }
+
+}
+
+void top_view(Node node, boolean goLeft) {
+
+    if(node != null) {
+        if(goLeft) {
+            top_view(node.left, goLeft);
+            System.out.print(node.data + " ");
+        } else {
+            System.out.print(node.data + " ");
+            top_view(node.right, goLeft);
+        }
+    } 
+
+}
