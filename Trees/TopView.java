d82aeec099267347fd49d744538972f93e9b98a7
Josh Yang <Jpy@uoregon.edu>
2016-11-09 19:11:52 -0800

    Happy numbers

--- Trees/TopView.java
+++ Trees/TopView.java
@@ -1,7 +1,7 @@
 /*  ----------------------------------------------------------------------------------------------------------------/                                                                                                               
 /   Problem: Print all left nodes from bottom up, followed by all right node from top down                          /
 /                                                                                                                   /
-/   Node is defined as:                                                                                               /
+/   Node is defined as:                                                                                             /
 /           class Node {                                                                                            /
 /             int data;                                                                                             /
 /             Node left;                                                                                            /
