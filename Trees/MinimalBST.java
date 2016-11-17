36ceee1acb164774271a5e395db11f88ff3af074
Josh Yang <Jpy@uoregon.edu>
2016-11-16 22:04:17 -0800

    Push

--- Trees/MinimalBST.java
+++ Trees/MinimalBST.java
@@ -1,1 +1,28 @@
 /*  ----------------------------------------------------------------------------------------------------------------/                                                                                                               
+/   Problem: Given a sorted array of integers, create a BST with minimal height			                            /
+/                                                                                                                   /
+/   Node is defined as:                                                                                               /
+/           class Node {                                                                                            /
+/             int data;                                                                                             /
+/             Node left;                                                                                            /
+/             Node right;                                                                                           /
+/           }                                                                                                       /
+/   ---------------------------------------------------------------------------------------------------------------*/
+
+// Create a BST with minimal height
+public TreeNode createMinimalBST(int[] array){
+	return createMinimalBST(array, 0, array.length - 1);
+}
+
+public TreeNode createMinimalBST(int[] array, start, end){
+	if(end < start){
+		return null;
+	}
+
+	int mid = (end + start)/2;
+	TreeNode n = new TreeNode(array[mid]);
+	n.left = createMinimalBST(array, start, mid - 1);
+	n.right = createMinimalBST(array, mid + 1, end);
+
+	return n;
+}
