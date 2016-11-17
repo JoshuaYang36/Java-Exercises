ff2e8afe94e0c2dc5253286aa065d72717d677cf
Josh Yang <Jpy@uoregon.edu>
2016-11-16 22:03:03 -0800

    TopDownView BST

--- Trees/TopView.java
+++ Trees/TopView.java
@@ -15,24 +15,5 @@ void top_view(Node root)
 
         top_view(root.left, true);
 
-        System.out.print(root.data + " ");
-
-        top_view(root.right, false);
-
-    }
-
-}
-
-void top_view(Node node, boolean goLeft) {
-
-    if(node != null) {
-        if(goLeft) {
-            top_view(node.left, goLeft);
-            System.out.print(node.data + " ");
-        } else {
-            System.out.print(node.data + " ");
-            top_view(node.right, goLeft);
-        }
-    } 
-
+  
 }
